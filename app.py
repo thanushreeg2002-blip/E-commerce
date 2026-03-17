37ab819dfda462a9b8fc1f685f265249306d782b
thanushree <thanushreeg2002@gmail.com>
2026-03-17 12:09:41 +0530

    created landing page

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="E-Commerce App")
+
+st.title("🛒 Welcome to My E-Commerce Store")
+
+st.write("Shop the best products online!")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("Sign Up"):
+        st.switch_page("pages/signup.py")
+
+with col2:
+    if st.button("Sign In"):
+        st.switch_page("pages/login.py")
